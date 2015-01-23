3a44846d51354a9cf724563b275bfe8cd12f6ee8
shsalt <shepards.alt@gmail.com>
2015-01-22 20:26:06 -0800

    clear

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){  //modal on-click opening
-    var $resume_pdf = $('#resume-modal').modal({
-        show: false
-    });
-    $('#resume').on('click', function() {
-        $resume_pdf.modal('show');
-    });
-    
-});    
--- assets/js/modal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){  //modal on-click opening
-    var $resumepdf = $('#resume-modal').modal({
-        show: false
-    });
-    $('#resume').on('click', function() {
-        $resumepdf.modal('show');
-    });
-});    
