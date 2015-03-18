d3de0e05f0b7f091e2f5ebf74ff75be1ba5e4702
shsalt <shepards.alt@gmail.com>
2015-03-17 22:48:56 -0700

    so much CONTENT

--- assets/js/scrollto.js
+++ assets/js/scrollto.js
@@ -1,14 +1,1 @@
-$(function() {
- 	  $('a[href*=#]:not([href=#])').click(function() {
- 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
- 	      var target = $(this.hash);
- 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
- 	      if (target.length) {
- 	        $('html,body').animate({
- 	          scrollTop: target.offset().top
- 	        }, 1000);
- 	        return false;
- 	      }
- 	    }
-    });
-});
+$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var a=$(this.hash);if(a=a.length?a:$("[name="+this.hash.slice(1)+"]"),a.length)return $("html,body").animate({scrollTop:a.offset().top},1e3),!1}})});
